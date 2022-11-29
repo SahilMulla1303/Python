f5add64e6bf743dc8a028a4188d4412c0752fdbd
Sahil Mulla <sahilmulla1672@gmail.com>
2022-11-29 09:27:39 +0530

    Assignment 13

---
+++ Assignment14/Function_programs.py
@@ -0,0 +1,39 @@
+
+def sum():
+    a=10
+    b=30
+    print("Sum is :",a+b)
+
+def sub():
+    a=60
+    b=30
+    print("Sub is :",a-b)
+    
+def mul():
+    a=45
+    b=6
+    print("Mul is :",a*b)
+
+def div():
+    a=68
+    b=7
+    print("Div is :",a/b)
+
+def Fdiv():
+    a=68
+    b=7
+    print("Floor division is :",a//b)
+
+def mod():
+    a=47
+    b=3
+    print("Remainder is :",a%b)
+
+
+    
+sum()
+sub()
+mul()
+div()
+Fdiv()
+mod()
